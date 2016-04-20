ca05ee77c6252906d92197571b7bdbd0e8143eee
Chris Mytton <chrismytton@gmail.com>
2016-04-20 10:48:44 +0200

    Working version

--- lib/ocd_lookup.rb
+++ lib/ocd_lookup.rb
@@ -1,5 +1,24 @@
 require 'ocd_lookup/version'
 
 module OcdLookup
-  # Your code goes here...
+  class DivisionId
+    attr_reader :mapping
+
+    def initialize(rows)
+      @mapping = Hash[rows.map { |r| [r[:name], r[:id]] }]
+    end
+
+    # Figure out what order we should search the types in.
+    def priority
+      @priority ||= mapping.values.map do |t|
+        t.split('/').drop(1).map { |p| p.split(':').first }.map(&:to_sym)
+      end.uniq
+    end
+
+    def find(opts)
+      ids = opts.map { |type, val| id = mapping[val]; id if id && id.split('/').last.start_with?(type.to_s) }
+      return if ids.any?(&:nil?)
+      ids.reduce { |prev, id| prev && prev.start_with?(id) ? prev : id }
+    end
+  end
 end
--- test/ocd_lookup_test.rb
+++ test/ocd_lookup_test.rb
@@ -1,11 +1,30 @@
 require 'test_helper'
+require 'pry'
+require 'csv'
 
 class OcdLookupTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::OcdLookup::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def country_au_mapping
+    CSV.parse(File.read('test/fixtures/country-au.csv'), headers: true, header_converters: :symbol).map(&:to_hash)
+  end
+
+  def subject
+    @lookup ||= OcdLookup::DivisionId.new(country_au_mapping)
+  end
+
+  def test_looking_up_country
+    assert_equal 'ocd-division/country:au', subject.find(country: 'Australia')
+  end
+
+  def test_looking_up_state
+    assert_equal 'ocd-division/country:au/state:nsw', subject.find(state: 'New South Wales')
+  end
+
+  def test_looking_up_country_and_state
+    assert_equal  'ocd-division/country:au/state:nsw', subject.find(country: 'Australia', state: 'New South Wales')
+    assert_equal nil, subject.find(country: 'Canada', state: 'New South Wales')
   end
 end
